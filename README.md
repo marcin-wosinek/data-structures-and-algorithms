34171f2d6025145664eee645d04e55af09bdbb3b
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-11-04 18:44:28 +0100

    add simple calculator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # data-structures-and-algorithms
+
+Excercises form https://www.amazon.com/Structures-Algorithms-Python-Michael-Goodrich/dp/1118290275
---
+++ chapter-1/P-1.32.py
@@ -0,0 +1,44 @@
+
+def equal(memory, value):
+  return value
+ 
+def sum(memory, value):
+  return memory + value
+ 
+def multiply(memory, value):
+  return memory * value
+
+def divide(memory, value):
+  return memory / value
+
+def rest(memory, value):
+  return memory - value
+
+options = {
+  '': equal,
+  '=': equal,
+  '+': sum,
+  '*': multiply,
+  '/': divide,
+  '-': rest
+}
+
+play = True
+
+memory = 0
+operation = None
+
+while play:
+
+  value = input()
+
+  if (value.isdecimal()):
+    if operation is None:
+      memory = float(value)
+    else:
+      memory = operation(memory, float(value))
+  else:
+    operation = options[value]
+
+  print(memory)
+
